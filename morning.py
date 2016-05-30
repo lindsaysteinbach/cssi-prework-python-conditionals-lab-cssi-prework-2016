cd69c9e4003ba8663a70d61c12950b31a0b9931e
lindsaysteinbach <lsteinbach@google.com>
2016-05-30 19:18:55 -0400

    Done.

--- morning.py
+++ morning.py
@@ -1,18 +1,30 @@
 
 #1. Waking Up
-def wake_up():
-    #write your conditional statement here.
-    return "replace this line with all of your code!"
-
+def wake_up(day_of_week):
+    if ((day_of_week.lower() == "saturday") or (day_of_week.lower() == "sunday")):
+      return "Go back to bed"
+    else:
+      return "Stop hitting snooze"
 
 #2. The Commute
-def commute():
-    #write your conditional statement here
-    return "replace this line with all of your code!"
-
-
+def commute(weather, mins_until_work):
+    if ((mins_until_work<=10) or (weather.lower()=="rainy")):
+      return "Better take the car"
+    elif ((mins_until_work>30) and (weather.lower()=="sunny")):
+      return "Enjoy a bike ride!"
+    else:
+      return "Hop on the bus"
 
 #3. Coffee Buzz
-def coffee():
-    #write your conditional statement here
-    return "replace this line with all of your code!"
+
+def coffee(number):
+  if (number>=100):
+    return "Time for a triple shot espresso"
+  elif ((number<100) and (number>=50)):
+    return "Go for a large black coffee"
+  elif ((number<50) and (number>0)):
+    return "A latte will serve you well"
+  elif (number == 0):
+    return "You have reached the Nirvana of 21st century communication!"
+  elif (number < 0):
+    return "Is this a new Gmail feature?"
